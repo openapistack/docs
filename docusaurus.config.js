5be68d05180334b6aa5de6cb6d4ce89e5bd6ad6c
Viljami <v.kuosmanen@epilot.cloud>
2023-03-05 12:14:26 +0100

    Typo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/anttiviljami/opanapi-stack/edit/main/',
+          editUrl: 'https://github.com/anttiviljami/openapi-stack/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
