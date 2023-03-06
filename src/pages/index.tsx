e29e264c13e28b3e13799c43dbe75304df9a28d9
Viljami <v.kuosmanen@epilot.cloud>
2023-03-06 15:52:26 +0100

    Fix CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,16 +12,18 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container max-w-2xl">
-        <h1 className="hero__title text-6xl">Full stack typesafe API-first development for REST.</h1>
-        <p className="hero__subtitle">OpenAPI Stack is a collection of open source libraries and tools for full stack Typescript development inspired by modern alternatives like GraphQL and tRPC.</p>
-        <div className="space-x-2">
-          <GithubStarsButton />
-          <Link
-            className="button button--secondary button--lg mb-2"
-            to="/docs/intro">
-              Documentation
-          </Link>
+      <div className="container flex justify-center">
+        <div className="max-w-2xl">
+          <h1 className="hero__title">Full stack typesafe API-first development for REST.</h1>
+          <p className="hero__subtitle">OpenAPI Stack is a collection of open source libraries and tools for full stack Typescript development inspired by modern alternatives like GraphQL and tRPC.</p>
+          <div className="space-x-2">
+            <GithubStarsButton />
+            <Link
+              className="button button--secondary button--lg mb-2"
+              to="/docs/intro">
+                Documentation
+            </Link>
+          </div>
         </div>
       </div>
     </header>
