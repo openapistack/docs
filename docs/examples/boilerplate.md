45f4c7b73f426855a149f56183d50b2f02f49d51
Viljami <v.kuosmanen@epilot.cloud>
2023-10-10 10:02:20 +0300

    Reorder examples

--- docs/examples/boilerplate.md
+++ docs/examples/boilerplate.md
@@ -1,6 +1,6 @@
 ---
 title: Boilerplate projects
-sidebar_position: 0
+sidebar_position: 10
 ---
 
 :::tip
--- src/pages/docs/examples.tsx
+++ src/pages/docs/examples.tsx
@@ -2,7 +2,7 @@ import { Redirect } from '@docusaurus/router';
 import React from 'react';
 
 function RedirectPage(): JSX.Element {
-  return <Redirect to="/docs/examples/boilerplate" />;
+  return <Redirect to="/docs/examples/building-apis" />;
 }
 
 export default RedirectPage;
